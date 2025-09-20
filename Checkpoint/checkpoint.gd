951785565f720cbc19d11f253c0f20871c1384fc
aryamanmajumdar <aryaman.cis@gmail.com>
2025-09-20 16:02:03 +0530

    Update checkpoint.gd

--- Checkpoint/checkpoint.gd
+++ Checkpoint/checkpoint.gd
@@ -3,10 +3,15 @@ extends Area2D
 class_name Checkpoint
 
 @onready var _flag: = $Flag
+
 @onready var _marker:Marker2D = $FlagMarker
 @onready var _sfx:AudioStreamPlayer2D = $sfx
 @onready var _fx:CPUParticles2D = $fx
+
+@export var side: float = 100.0
+
 var activated = false
+
 
 func _ready():
 	CheckpointManager.register_checkpoint(self)
@@ -20,8 +25,14 @@ func _on_body_entered(body: Node2D) -> void:
 		activate_flag()
 
 func activate_flag():
+	
+	$FIRE.emitting = true
+	$Flag.color.a = 1
+
+	
 	_sfx.play()
 	_fx.emitting = true
 	var tween = create_tween()
-	tween.tween_property(_flag, "position", _marker.position, 0.5).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
-	tween.tween_property(_flag, "modulate", Color.GREEN, 0.5).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	
+	#tween.tween_property(_flag, "position", _marker.position, 0.5).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	tween.tween_property(_flag, "modulate", Color(2.265, 0.773, 0.342), 0.5).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
